44e229a34a199e82b1dfa55103d676e231e28068
Cameron Downer <camerondowner@gmail.com>
2020-07-21 21:11:25 -0500

    updated dependencies removed angular todoApp from projects added liveUrl and mockup image to PokeDirectory projectItem

--- src/components/ProjectsSection.vue
+++ src/components/ProjectsSection.vue
@@ -55,9 +55,10 @@
           {
             id: 1,
             name: "PokeDirectory",
-            mockupFileName: "project_coming_soon.jpg",
-            description: "An Angular webapp for the purpose of browsing and viewing the stats of your favorite pokemon. All pokemon data is retrieved via the RESTful PokeApi. Click the Github button below to follow the development process and view all commits thus far.",
+            mockupFileName: "PokeDirectory.jpg",
+            description: "An Angular webapp for the purpose of browsing and viewing the stats of your favorite pokemon. All pokemon data is retrieved via the RESTful PokeApi. State data is efficiently managed with NgRx.",
             githubUrl: "https://github.com/CameronRDowner/Angular_PokeDirectory",
+            liveUrl: "https://camerondowner.com/Angular_PokeDirectory/",
             technologiesList: [
                 {
                   id: 0,
@@ -83,65 +84,6 @@
           },
           {
             id: 2,
-            name: "Wescot Roofing Website",
-            mockupFileName: "wescot_roofing_website.jpg",
-            description: "This website began with the Elevation template from pixilarity.com which was then heavily modified to meet the branding and functional needs of the client. CSS animations were added along with a scroll event handler to activate them. Additionally, field validation was integrated into the contact form to ensure accurate submissions.",
-            liveUrl: "https://www.wescotroofing.com",
-            technologiesList: [
-                {
-                  id: 0,
-                  deviconClasses: "devicon-html5-plain colored"
-                },
-                {
-                  id: 1,
-                  deviconClasses: "devicon-css3-plain colored"
-                },
-                {
-                  id: 2,
-                  deviconClasses: "devicon-javascript-plain colored"
-                },
-                {
-                  id: 3,
-                  deviconClasses: "devicon-jquery-plain colored"
-                },
-                {
-                  id: 4,
-                  deviconClasses: "devicon-sass-plain colored"
-                }
-            ],
-          },
-          {
-            id: 3,
-            name: "Todo List App",
-            mockupFileName: "todo_app.jpg",
-            description: "A todo list app built in Angular that can Add, Delete, and Complete todo list items. All todo items are retrieved from a REST API using the RxJS library. CSS animations were handled via Angular's built-in Browser Animations module. Many of the common Angular directives were utilized in the markup of this project.",
-            githubUrl: "https://github.com/CameronRDowner/Angular_Todo_App",
-            liveUrl: "https://camerondowner.com/Angular_Todo_App/",
-            technologiesList: [
-              {
-                  id: 0,
-                  deviconClasses: "devicon-html5-plain colored"
-                },
-                {
-                  id: 1,
-                  deviconClasses: "devicon-css3-plain colored"
-                },
-                {
-                  id: 2,
-                  deviconClasses: "devicon-javascript-plain colored"
-                },
-                {
-                  id: 3,
-                  deviconClasses: "devicon-angularjs-plain colored"
-                },
-                {
-                  id: 4,
-                  deviconClasses: "devicon-sass-plain colored"
-                }
-            ],
-          },
-          {
-            id: 4,
             name: "Portfolio Website",
             mockupFileName: "portfolio_website.jpg",
             description: "The portfolio page you are currently browsing is built with Vue. Many of the components that make up this page utilize Vue directives, Flexbox and Grid. When appropriate, animations use the Vue transition wrapper component for triggering; all others utilize a scroll event handler. And lastly, the contact form uses a validation library to ensure accuracy.",
@@ -169,7 +111,36 @@
                   deviconClasses: "devicon-sass-plain colored"
                 }
             ],
-          }
+          },
+          {
+            id: 3,
+            name: "Wescot Roofing Website",
+            mockupFileName: "wescot_roofing_website.jpg",
+            description: "This website began with the Elevation template from pixilarity.com which was then heavily modified to meet the branding and functional needs of the client. CSS animations were added along with a scroll event handler to activate them. Additionally, field validation was integrated into the contact form to ensure accurate submissions.",
+            liveUrl: "https://www.wescotroofing.com",
+            technologiesList: [
+                {
+                  id: 0,
+                  deviconClasses: "devicon-html5-plain colored"
+                },
+                {
+                  id: 1,
+                  deviconClasses: "devicon-css3-plain colored"
+                },
+                {
+                  id: 2,
+                  deviconClasses: "devicon-javascript-plain colored"
+                },
+                {
+                  id: 3,
+                  deviconClasses: "devicon-jquery-plain colored"
+                },
+                {
+                  id: 4,
+                  deviconClasses: "devicon-sass-plain colored"
+                }
+            ],
+          },
         ]
       }
     },
